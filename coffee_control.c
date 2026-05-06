60d21d5dc827b75a43b2051ba4f4260f765e3c5a
Silas Tvingsholm <sat@exofix.com>
2026-05-06 12:36:23 +0200

    Select payment added

--- coffee_control.c
+++ coffee_control.c
@@ -2,86 +2,127 @@
 #include "LCD_frt.h"
 #include "keypad_frt.h"
 
-uint32_t get_runtime(){
-    return xTaskGetTickCount() / configTICK_RATE_HZ;
+uint32_t get_runtime()
+{
+  return xTaskGetTickCount() / configTICK_RATE_HZ;
 }
 
-timestamp_t get_timestamp(){
-    uint32_t all_seconds = get_runtime(); //get total amount of run time seconds since
+timestamp_t get_timestamp()
+{
+  uint32_t all_seconds = get_runtime(); // get total amount of run time seconds since
 
-    if(all_seconds > SEC_IN_DAY){  //we only need the time of day, if we run for more than one day
-        all_seconds -= SEC_IN_DAY; //remove the extra day
-    }
+  if (all_seconds > SEC_IN_DAY)
+  {                            // we only need the time of day, if we run for more than one day
+    all_seconds -= SEC_IN_DAY; // remove the extra day
+  }
 
-    timestamp_t return_val;
-    return_val.sec = all_seconds % TIME_BASE;
-    return_val.min = (all_seconds / TIME_BASE) % TIME_BASE;
-    return_val.hr  = all_seconds / (TIME_BASE*TIME_BASE);
+  timestamp_t return_val;
+  return_val.sec = all_seconds % TIME_BASE;
+  return_val.min = (all_seconds / TIME_BASE) % TIME_BASE;
+  return_val.hr = all_seconds / (TIME_BASE * TIME_BASE);
 
-    return return_val;
+  return return_val;
 }
 
 const char *options[] = {
     "1: ESPRESSO",
     "2: LATTE   ",
-    "3: FILTER  "
-};
+    "3: FILTER  "};
 
+const char *payments[] = {
+    "1: CARD",
+    "2: CASH"};
 
-void select_product(MACHINE_STATES_t* state_p, COFFEE_t* select_product_p){
-    char key;
-    char msg = CLEAR_LCD;
-    xQueueSend(lcd_queue, &msg, 1000);
-    lcd_queueString("SELECT PRODUCT:");
-    msg = 16; //command to go to start of second line
-    int current_option = 0;
+void select_product(MACHINE_STATES_t *state_p, COFFEE_t *select_product_p)
+{
+  char key;
+  char msg = CLEAR_LCD;
+  xQueueSend(lcd_queue, &msg, 1000);
+  lcd_queueString("SELECT PRODUCT:");
+  msg = 16; // command to go to start of second line
+  int current_option = 0;
 
-    while(*state_p==SEL_PRODUCT){
-        xQueueSend(lcd_queue, &msg, 1000); // go to start of second line
-        lcd_queueString(options[current_option]);
+  while (*state_p == SEL_PRODUCT)
+  {
+    xQueueSend(lcd_queue, &msg, 1000); // go to start of second line
+    lcd_queueString(options[current_option]);
 
-        //wait for key or one second timeout
-        if(xQueueReceive(keypad_queue, &key, pdMS_TO_TICKS(1000)) == pdPASS){
-            switch(key){
-                case '1':
-                    *selected_product_p = ESPRESSO;
-                    *state_p = SEL_PAYMENT;
-                    break;
-                case '2':
-                    *selected_product_p = LATTE;
-                    *state_p = SEL_PAYMENT;
-                    break;
-                case '3':
-                    *selected_product_p = FILTER;
-                    *state_p = SEL_PAYMENT;
-                    break;
-                default:
-                    break;
-            }
-        }else{
-            current_option = (current_option + 1) % 3;
-        }
+    // wait for key or one second timeout
+    if (xQueueReceive(keypad_queue, &key, pdMS_TO_TICKS(1000)) == pdPASS)
+    {
+      switch (key)
+      {
+      case '1':
+        *select_product_p = ESPRESSO;
+        *state_p = SEL_PAYMENT;
+        break;
+      case '2':
+        *select_product_p = LATTE;
+        *state_p = SEL_PAYMENT;
+        break;
+      case '3':
+        *select_product_p = FILTER;
+        *state_p = SEL_PAYMENT;
+        break;
+      default:
+        break;
+      }
     }
+    else
+    {
+      current_option = (current_option + 1) % 3;
+    }
+  }
 }
 
+void select_payment(MACHINE_STATES_t *state_p)
+{
+  char key;
+  char msg = CLEAR_LCD;
+  xQueueSend(lcd_queue, &msg, 1000);
+  lcd_queueString(payments[0]);
+  msg = 16; // command to go to start of second line
+  xQueueSend(lcd_queue, &msg, 1000);
+  lcd_queueString(payments[1]);
 
-void userflow_Task(void *pvParameters){
-    MACHINE_STATES_t state = SEL_PRODUCT;
-    COFFEE_t selected_product;
-
-    char key;
-    char msg;
-
-    while(1){
-        switch (state){
-        case SEL_PRODUCT:
-            select_product(&state, &selected_product);
-            break;
-        case SEL_PAYMENT:
-
-        }
-
+  while (*state_p == SEL_PAYMENT)
+  {
+    // check key every second
+    if (xQueueReceive(keypad_queue, &key, pdMS_TO_TICKS(1000)) == pdPASS)
+    {
+      switch (key)
+      {
+      case '1':
+        *state_p = ENTER_CARD;
+        break;
+      case '2':
+        *state_p = ENTER_CASH;
+        break;
+      default:
+        break;
+      }
     }
+  }
+}
 
+void userflow_Task(void *pvParameters)
+{
+  MACHINE_STATES_t state = SEL_PRODUCT;
+  COFFEE_t selected_product;
 
+  char key;
+  char msg;
+
+  while (1)
+  {
+    switch (state)
+    {
+    case SEL_PRODUCT:
+      select_product(&state, &selected_product);
+      break;
+    case SEL_PAYMENT:
+      select_payment(&state);
+      break;
+    }
+  }
 }
